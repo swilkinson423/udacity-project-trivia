4461d54dd237076460443a5176c6ea6d02d8fcd9
Scott Wilkinson <swilkinson423@gmail.com>
2023-03-07 08:25:54 +0100

    added unit test for GET questions by category

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -59,6 +59,16 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data["success"], True)
         self.assertTrue(data["categories"])
     
+    def test_get_questions_by_category(self):
+        res = self.client().get("/categories/2/questions")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["total_questions"])
+        self.assertTrue(len(data["questions"]))
+        self.assertTrue(data["current_category"], 2)
+
 
 
     # TODO: TEST FOR GET QUESTIONS IN CATEGORY
